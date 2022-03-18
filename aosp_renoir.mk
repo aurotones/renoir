08a02f4426fde683954cf70e15cbdf603e6bd96b
kleidione Freitas <kleidione@gmail.com>
2022-03-18 18:38:56 -0300

    renoir: Start bringup pixelexperience
    Signed-off-by: kleidione <kleidione@gmail.com>

--- aosp_renoir.mk
+++ aosp_renoir.mk
@@ -8,16 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Aosp recovery
+TARGET_USES_AOSP_RECOVERY := true
+
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common pixelexperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := aosp_renoir
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
